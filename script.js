b97342c1f698e94f81d59ed64ab0382cb65998af
Muhammad Mohsin <141544241+mmohsin18@users.noreply.github.com>
2023-12-26 11:43:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let SHEET_ID = '10PEpj1wra80e9yJbwjMy9mn4J-M8Nr29ycHRHWrZeEo'
+let SHEET_ID = '138AkMo3HwXfvEouNgPANlLRGBnZrK_WpTK8M2gjg5Gk'
 let SHEET_TITLE = 'Form Responses 1';
 let SHEET_RANGE ='A2:G100';
 
@@ -14,7 +14,8 @@ fetch(FULL_URL)
     let card2 = document.getElementById('card2');
     let cardh1 = document.getElementById('cardh1');
     let cardh5 = document.getElementById('cardh5');
-    let cardimg = document.getElementById('cardimg');
+    let cardh6 = document.getElementById('cardh6');
+    let carda = document.getElementById('carda');
 
     // let dt_title = document.getElementById('dt_title');
     // let dt_description = document.getElementById('dt_description');
@@ -25,36 +26,43 @@ fetch(FULL_URL)
 
     cardh1.innerHTML = data.table.rows[0].c[3].v
     cardh5.innerHTML = data.table.rows[0].c[5].v
-    cardimg.src = data.table.rows[0].c[6].v
+    cardh6.innerHTML = "By, " + data.table.rows[0].c[2].v
+    carda.href = data.table.rows[0].c[4].v
+    console.log(data.table.rows[0].c[4].v);
 
     for(let i = 1; i<length; i++){
                 // Assuming you have a reference to your NewBox div
         var NewBox = document.createElement('div');
         NewBox.id = ("box" + i);
-        NewBox.className = ("carding")
+        NewBox.className = ("card")
         card2.append(NewBox);
 
         // // Create an h6 element
-        var img = document.createElement('img');
-        img.className ="cardimg"
         var h1 = document.createElement('h1');
         h1.className ="cardh1"
         var h5 = document.createElement('h5');
         h5.className ="cardh5"
-        var h52 = document.createElement('h5');
-        h52.className ="cardh52"
+        var h6 = document.createElement('h6');
+        h6.className ="cardh6"
+        var div = document.createElement('div');
+        div.className ="carding"
+        var a = document.createElement('a');
+        a.className ="carda"
 
-        // Set the content for the h2 element (you can replace "Your Heading" with the actual heading text)
-        console.log(data.table.rows[i].c[5].v)
+    //     // Set the content for the h2 element (you can replace "Your Heading" with the actual heading text)
+    //     // console.log(data.table.rows[i].c[5].v)
 
         h1.innerHTML = data.table.rows[i].c[3].v;
         h5.innerHTML = data.table.rows[i].c[5].v;
-        h52.innerHTML = data.table.rows[i].c[4].v;
+        h6.innerHTML = "By, " + data.table.rows[0].c[2].v
+        a.href = data.table.rows[i].c[4].v;
+        a.innerHTML = "Visit Website";
 
         // Append the h2 element to the NewBox div
         NewBox.appendChild(h1);
-        NewBox.appendChild(h5);
-        NewBox.appendChild(h52);
-        NewBox.appendChild(img);
+        div.appendChild(h5);
+        div.appendChild(h6);
+        div.appendChild(a);
+        NewBox.appendChild(div);
     }
 });
